d54734605212cec036e35f4ba5408d5400bab6aa
RosieOh <dhxogns920@gmail.com>
2025-01-27 03:12:31 +0900

    CHORE : Prettier로 오타 3차 수정

--- components/Utterances.tsx
+++ components/Utterances.tsx
@@ -8,7 +8,7 @@ function Utterances() {
         const scriptElement = document.createElement('script');
         scriptElement.src = 'https://utteranc.es/client.js';
         scriptElement.async = true;
-        scriptElement.setAttribute('repo', 'RosieOh/rosieBlog');
+        scriptElement.setAttribute('repo', 'RosieOh/RosieTechBlog');
         scriptElement.setAttribute('issue-term', 'pathname');
         scriptElement.setAttribute('theme', 'boxy-light');
         scriptElement.setAttribute('crossorigin', 'anonymous');
