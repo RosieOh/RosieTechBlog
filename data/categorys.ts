7585ecfd51045e23c3886baa1095dafca1ee9574
RosieOh <dhxogns920@gmail.com>
2025-01-29 16:25:41 +0900

    FEAT : 카테고리 추가

--- data/categorys.ts
+++ data/categorys.ts
@@ -1,8 +1,10 @@
 export const categorys: { title: string; keyword: string }[] = [
   { title: '🐱 All', keyword: '' },
   { title: '📖 JavaScript', keyword: 'javascript' },
+  { title: '회사생활', keyword: 'company' },
   { title: '🧑🏻‍💻 회고', keyword: 'retrospection' },
   { title: '🖥 Web', keyword: 'web' },
+  { title: 'Server', keyword: 'server'},
   { title: '👨🏻‍🏫 React', keyword: 'react' },
   { title: '⚙️ Git', keyword: 'git' },
   { title: '📝 정리', keyword: 'arrangement' },
