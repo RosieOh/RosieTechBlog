6aa0870a4e3e15fa2eb59a5b3f3bc390c34f78bb
RosieOh <dhxogns920@gmail.com>
2025-05-04 18:52:08 +0900

    STYLE : Layout 스타일 개선

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import Footer from './Footer';
 import Header from './Header';
+import SplashScreen from './SplashScreen';
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,6 +10,7 @@ interface LayoutProps {
 export default function Layout(props: LayoutProps) {
   return (
     <>
+      <SplashScreen />
       <Header />
       <main className="transition duration-500 min-h-screen bg-white dark:bg-[#111111] text-black dark:text-white">
         <div className="max-w-screen-md flex flex-col px-10 m-auto">{props.children}</div>
